d14c422ecd1e326dab17c1fd290e741a3030d7e6
Christophe Mannebarth <c.i.s.carpentras@gmail.com>
2020-12-02 13:13:04 +0100

    Add files via upload

---
+++ js/Voiture.js
@@ -0,0 +1,120 @@
+/*
+Ceci est la classVoiture 
+utilisée par le programme parcAuto
+*/
+class Car {
+  constructor(registration, color, weight, power, capacity, places) {
+    this.registration = registration;
+    this.color = color;
+    this.weight = weight;
+    this.power = power;
+    this.capacity = capacity;
+    this.places = places;
+    this.fuel = 5;
+    this.insurance = false;
+    // this.newColor = null;
+  }
+  //Message d'acceuil du tableau de bord
+  welcome() {
+    return `
+    Bienvenu dans votre nouveau véhicule`;
+  }
+  // Fiche technique
+  notice() {
+    if (this.insurance == false) {
+      return `
+    FICHE TECHNIQUE
+    Ce véhicule est immmatriculé `+reg.toString()+`
+    Il pèse ${this.weight} Kg pour une puissance de `+pow.toString()+` chevaux.
+    Il peut recevoir ${this.places} personnes et contenir ${this.capacity} litres d'essence.`;
+    } else {
+      return `
+    PARFAIT Ce véhicule est bien assuré
+    Vous pouvez démarrer`;
+    }
+  }
+  //checking de l'assurance
+  checkInsurance() {
+    if (this.insurance == false) {
+      return `NON`;
+    } else {
+      return `OUI`;
+    }
+  }
+  //Tableau de bord
+  dashboard() {
+    return `
+    TABLEAU DE BORD
+    Ce véhicule `+col.toString()+` contient ${this.fuel} litres d'essence
+    Présence d'assurance = ${this.checkInsurance()}
+    TAPEZ
+    1 pour changer de couleur de carrosserie
+    2 pour mettre de l'essence 
+    3 pour partir avec le véhicule
+    4 pour prendre une assurance
+    5 pour rentrer au garage (quitter le test)`;
+  }
+  // Repeindre la voiture
+  checkColor() {
+    return `
+    Le véhicule est actuellement ${this.color.toUpperCase()}
+    Veuillez definir une nouvelle couleur de carrosserie
+    ou la même couleur pour effectuer un rafraichissement`;
+  }
+  
+  repaint() {
+    if (this.newColor != this.color) {
+      this.color = this.newColor;
+      return `
+    Merci d'avoir changé la couleur du véhicule
+    Ce ${this.color} est magnifique!`; 
+    } else {
+      return `
+    Merci d'avoir rafraichi la couleur du véhicule!`
+    } 
+  }
+  // Faire l'appoint d'essence
+  remainFuel() {
+    return `
+    Il y a ${this.fuel} litres dans le reservoir 
+    pour une capacité de ${this.capacity} litres.
+    Combien de litres d'essence voulez-vous ajouter?`;
+  }
+  checkFuel() {
+    if (this.newFuel <= parseInt(this.capacity) - parseInt(this.fuel)) {
+      this.fuel = parseInt(this.newFuel) + parseInt(this.fuel);
+      return `
+    Ajout de ${this.newFuel} litres d'essence
+    Le réservoir contient maintenant ${this.fuel} litres`;
+    } else {
+      return `
+    Vous ne pouvez pas depasser la capacité maximale du réservoir!!.`;
+    }
+  }
+  // Se déplacer
+  move() {
+    if (this.speed < 50) {
+      this.consumption = (this.distance / 100) * 10;
+      console.log(`Vous allez consommer 10 litres aux 100km`);
+    } else if (50 <= this.speed && this.speed <= 90) {
+      this.consumption = (this.distance / 100) * 5;
+      console.log(`Vous allez consommer 5 litres aux 100km`);
+    } else if (91 <= this.speed && this.speed <= 130) {
+      this.consumption = (this.distance / 100) * 8;
+      console.log(`Vous allez consommer 8 litres aux 100km`);
+    } else if (131 <= this.speed) {
+      this.consumption = (this.distance / 100) * 12;
+      console.log(`Vous allez consommer 12 litres aux 100km`);
+    }
+    let possibleDist = (this.fuel / this.consumption) * 100;
+    if (this.distance >= possibleDist) {
+      return `
+    Vous n'avez pas assez d'essence pour effectuer ce trajet`;
+    } else {
+      this.fuel = parseFloat(this.fuel - this.consumption);
+      return `
+    Vous avez consommé ${this.consumption} litres, 
+    il vous reste: ${this.fuel} litres dans votre réservoir`;
+    }
+  }
+}
---
+++ js/main.js
@@ -0,0 +1,91 @@
+/*
+Ceci est le programme parcAuto
+fonctionnant avec la classVoiture
+*/
+let car1 = new Car("AA-229-CD", "BLEU", 1576, 90, 45, 5);
+let reg = new String("AA-229-CD");
+let col = new String("BLEU");
+let pow = new String("90");
+
+try {
+  car1.insurance;
+} catch (e) {
+  alert(e.message);
+}
+// console.log(car1.toString());
+console.log(car1.welcome());
+console.log(car1.notice());
+console.log(car1.dashboard());
+
+let choice = null;
+while (choice != 5) {
+  choice = prompt(`TAPEZ votre choix (1, 2, 3, 4 ou 5)`);
+  if (choice > 0 && choice <= 4) {
+    switch (choice) {
+      case "1": //Couleur du véhicule
+        console.log(`Vous sortez le pistolet à peinture`);
+        console.log(car1.checkColor());
+
+        let newColor = prompt("TAPEZ la couleur voulue");
+        console.log(`Vous peignez le véhicule en ${newColor.toUpperCase()}`)
+        car1.newColor = newColor.toUpperCase();
+
+        console.log(car1.repaint());
+        console.log(car1.dashboard());
+        break;
+
+      case "2": // Mettre de l'essence
+        console.log(`Vous remplissez le reservoir`);
+        console.log(car1.remainFuel());
+
+        let newFuel = prompt(`Tapez le nombre de litres d'essence désirés.`);
+        console.log(`Vous demandez ${newFuel} litres`);
+        car1.newFuel = newFuel;
+
+        console.log(car1.checkFuel());
+        console.log(car1.dashboard());
+        break;
+
+      case "3": // Se déplacer
+        if (car1.insurance == true) {
+          console.log(`Vous préparez votre itinéraire`);
+
+          let distance = prompt("Combien de Km voulez-vous parcourir?");
+          console.log(`Vous voulez faire ${distance} Km`);
+          car1.distance = distance;
+          let speed = prompt("Entrez votre vitesse en Km/h");
+          console.log(`En roulant en moyenne à ${speed} Km/h`);
+          car1.speed = speed;
+          let consumption = "";
+          car1.consumption = consumption;
+
+          console.log(car1.move());
+          console.log(car1.dashboard());
+        } else {
+          {
+            console.error(`
+        Vous vous êtes fait arreter par la police!
+        Vous recevez une amende de 90€ pour defaut d'assurance
+        Vous rentrez au garage (rafraichissez la page)`);
+          }
+        }
+        break;
+
+      case "4": // Prendre une assurance
+        if (car1.insurance == false) {
+          car1.insurance = true;
+          console.log(`Vous souscrivez à une assurance`);
+          console.log(car1.notice());
+          console.log(car1.dashboard());
+        } else {
+          alert(`Vous etes déja assuré!`);
+        }
+        break;
+    }
+  } else if (choice == 5) {
+    console.log(`
+    Vous rentrez au garage...
+    Merci d'avoir utilisé notre programme!
+    La ligue du Code vous salue bien!`);
+  }
+}
