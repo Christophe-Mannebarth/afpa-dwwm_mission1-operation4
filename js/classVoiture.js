eb6e23b313d2211ce0b38b30337da1108ad40a45
Christophe Mannebarth <c.i.s.carpentras@gmail.com>
2020-12-02 05:59:12 +0100

    Add files via upload

--- js/classVoiture.js
+++ js/classVoiture.js
@@ -1,141 +1,102 @@
 /*
-Ceci est la classe Voiture utilisée par le programme parcAuto
+Ceci est la classVoiture 
+utilisée par le programme parcAuto
 */
 class Car {
-  constructor(registration,color,weight,power,capacity,places,) {
-    this.registration = registration; 
-    this.color = color; 
-    this.weight = weight; 
-    this.power = power; 
-    this.capacity = capacity; 
-    this.places = places; 
-    this.fuel = 5; 
-    this.insurance = Boolean(false); 
-    this.message = "Bienvenu dans votre nouveau véhicule"; 
-    this.newFuel = 0;
-    this.distance = 0;
-    this.speed = 0;
-    this.consumption = 0;
+  constructor(registration, color, weight, power, capacity, places) {
+    this.registration = registration;
+    this.color = color;
+    this.weight = weight;
+    this.power = power;
+    this.capacity = capacity;
+    this.places = places;
+    this.fuel = 5;
+    this.insurance = false;
+    this.message = "Bienvenu dans votre nouveau véhicule";
   }
   //Message d'acceuil du tableau de bord
-  get welcome() {
-    return this.getWelcome();
-  }
-  getWelcome() {
+  welcome() {
     return this.message;
   }
   // Fiche technique
-  get notice() {
+  notice() {
     if (this.insurance == false) {
-    return this.getNotice1();
+      return `FICHE TECHNIQUE
+    Ce véhicule est immmatriculé ${this.registration}
+    Il pèse ${this.weight} Kg pour une puissance de ${this.power} chevaux.
+    Il peut recevoir ${this.places} personnes et contenir ${this.capacity} litres d'essence.`;
     } else if (this.insurance == true) {
-      return this.getNotice2(); 
+      return `
+    PARFAIT Ce véhicule est bien assuré
+    Vous pouvez démarrer`;
     }
   }
-  getNotice1() {
-    return `FICHE TECHNIQUE
-    Ce véhicule immmatriculé ${this.registration} est ${this.color}
-    Il pèse ${this.weight}Kg pour une puissance de ${this.power} chevaux.
-    Il peut recevoir ${this.places} personnes et contenir ${this.capacity} litres d'essence.
-    Il est fourni avec ${this.fuel} litres d'essence.
-    ATTENTION ce véhicule n'est pas assuré!`
-  }
-  getNotice2() {
-    return `
-    PARFAIT Ce véhicule est bien assuré
-    Vous pouvez démarrer`
-  }
-  //Assurance
-  set setInsurance(setInsurance) {
-    return this.insurance = setInsurance;
+  //checking de l'assurance
+  checkInsurance() {
+    if (this.insurance == false) {
+      return `NON`;
+    } else {
+      return `OUI`;
+    }
   }
   //Tableau de bord
-  get dashboard() {
-    return this.getDashboard();
-  }
-  getDashboard() {
-    return `TABLEAU DE BORD
-    Ce véhicule ${this.color} contient ${this.fuel}  litres d'essence
+  dashboard() {
+    return `
+    TABLEAU DE BORD
+    Ce véhicule ${this.color} contient ${this.fuel} litres d'essence
+    Présence d'assurance = ${this.checkInsurance()}
     TAPEZ
     1 pour changer de couleur de carrosserie
     2 pour mettre de l'essence 
     3 pour partir avec le véhicule
-    4 pour rentrer au garage (quitter le test)`;
+    4 pour prendre une assurance
+    5 pour rentrer au garage (quitter le test)`;
   }
   // Repeindre le véhicule
-  get logColor() {
-    return this.getLogColor();
-  }
-  getLogColor() {
+  checkColor() {
     return `
     Le véhicule est actuellement ${this.color}
     Veuillez definir une nouvelle couleur de carrosserie
-    ou la même couleur pour effectuer un rafraichissement`
-  }  
-  set setColor(setColor) {
-    return this.color = setColor;
+    ou la même couleur pour effectuer un rafraichissement`;
+  }
+  newColor() {
+    return `
+    Merci d'avoir changé la couleur du véhicule
+    Ce ${this.color} est magnifique!`;
+  }
+  sameColor() {
+    return `
+    Merci d'avoir rafraichi la couleur du véhicule!`;
   }
   //Remettre de l'essence
-  get logFuel() {
-    return this.getLogFuel();
-  }
-  getLogFuel() {
+  remainFuel() {
     return `
     Il y a ${this.fuel} litres dans le reservoir 
     pour une capacité de ${this.capacity} litres.
-    Combien de litres d'essence voulez-vous ajouter?`
+    Combien de litres d'essence voulez-vous ajouter?`;
   }
-  set addFuel(addFuel) {
-    return this.newFuel = addFuel;
-  }
-  get errorFuel() {
+  checkFuel() {
     if (this.newFuel <= parseInt(this.capacity) - parseInt(this.fuel)) {
       this.fuel = parseInt(this.newFuel) + parseInt(this.fuel);
-      return this.getOkFuel();
-    } else {
-        return this.getErrFuel();
-    }
-  }     
-  getOkFuel() {
-    return `
+      return `
     Ajout de ${this.newFuel} litres d'essence
-    Le réservoir contient maintenant ${this.fuel} litres`
-  }
-  getErrFuel() {
-    return `
-    Vous ne pouvez pas depasser la capacité maximale du réservoir!!.` 
-  }
-  set setFuel(setFuel) {
-    return this.fuel = parseInt(setFuel) + parseInt(this.fuel);
-  }
-  //Calculer la consommation d'essence
-  set distKm(distKm) {
-    return this.distance = distKm;
-  }
-  set speedKm(speedKm) {
-    return this.speed = speedKm;
-  }
-  set consumKm(consumKm) {
-    return this.consumption = consumKm;
+    Le réservoir contient maintenant ${this.fuel} litres`;
+    } else {
+      return `
+    Vous ne pouvez pas depasser la capacité maximale du réservoir!!.`;
+    }
   }
   //Se déplacer
-  get move() {
+  move() {
     let possibleDist = (this.fuel / this.consumption) * 100;
     if (this.distance >= possibleDist) {
-        return this.getNoMove();
+      return `
+    Vous n'avez pas assez d'essence pour effectuer ce trajet`;
     } else {
-        this.fuel = parseFloat(this.fuel - this.consumption);
-        return this.getMove();
+      this.fuel = parseFloat(this.fuel - this.consumption);
+      return `
+    Vous avez consommé ${this.consumption} litres, 
+    il vous reste: ${this.fuel} litres dans votre réservoir`;
     }
   }
-  getNoMove() {
-    return `
-    Vous n'avez pas assez d'essence pour effectuer ce trajet`
-  }
-  getMove() {
-    return `
-    Vous avez consommé ${this.consumption} litres, il vous reste: ${this.fuel} litres dans votre réservoir`
-  }
 }
-
-
--- js/parcAuto.js
+++ js/parcAuto.js
@@ -1,142 +1,106 @@
 /*
-Ceci est le programme parAuto
+Ceci est le programme parcAuto
+fonctionnant avec la classVoiture
 */
 let car1 = new Car("AA-229-CD", "bleu", 1576, 90, 45, 5);
 
 try {
-  car1.setInsurance; 
+  car1.setInsurance;
 } catch (e) {
   alert(e.message);
 }
 
-console.log(car1.welcome);
-console.log(car1.notice);
+console.log(car1.welcome());
+console.log(car1.notice());
+console.log(car1.dashboard());
 
-let choice = parseInt(0);
-
-while (choice != 1) {
-  choice = prompt(`TAPEZ 1 pour prendre une assurance`);
-  //Si 1 est rentré
-  if (choice == 1) {
-    //On défini l'assurance sur true via le Setter setInsurance
-    car1.setInsurance = Boolean(true);
-    /*on affiche dans la console un message de confirmation
-    et on quitte la boucle*/
-    console.log(car1.notice);
-
-    /*si autre chose que 1 est rentré on affiche dans
-    la console un message d'erreur*/
-  } else {
-    {throw new Error(`
-    Vous vous êtes fait arreter par la police!
-    Vous recevez une amende de 90€ pour defaut d'assurance
-    Vous rentrez au garage (rafraichissez la page)`) ;}
-  }
-}
-
-console.log(car1.dashboard);
-
-while (choice != 4) {
-  choice = prompt(`TAPEZ votre choix (1, 2, 3 ou 4)`);
-  if (choice > 0 && choice <= 3) {
+let choice = null;
+while (choice != 5) {
+  choice = prompt(`TAPEZ votre choix (1, 2, 3, 4 ou 5)`);
+  if (choice > 0 && choice <= 4) {
     switch (choice) {
-      case "1":
+      case "1": //Couleur du véhicule
         console.log(`Vous sortez le pistolet à peinture`);
-        console.log(car1.logColor);
-        
+        console.log(car1.checkColor());
+
         let newColor = prompt("TAPEZ la couleur voulue");
-        car1.setColor = newColor;
+        car1.color = newColor;
 
         if (newColor != this.color) {
           this.color = newColor;
-          if (
-            confirm(`
-              Merci d'avoir changé la couleur du véhicule
-              Ce ${this.color} est magnifique!
-              OK pour retourner au tableau de bord
-              ANNULER pour rentrer le vehicule au garage`)
-          ) {
-            console.log(car1.dashboard);
-          } else {
-            window.location.reload();
-          }
+          console.log(car1.newColor());
+          console.log(car1.dashboard());
         } else {
-          if (
-            confirm(`
-              Merci d'avoir rafraichi la couleur du véhicule!
-              OK pour retourner au tableau de bord
-              ANNULER pour rentrer le vehicule au garage`)
-          ) {
-            console.log(car1.dashboard);
-          } else {
-            window.location.reload();
-          }
+          console.log(car1.sameColor());
+          console.log(car1.dashboard());
         }
         break;
-      case "2":
+
+      case "2": // Mettre de l'essence
         console.log(`Vous remplissez le reservoir`);
-        console.log(car1.logFuel);
+        console.log(car1.remainFuel());
 
         let newFuel = 0;
         newFuel = prompt(`Tapez le nombre de litres d'essence désirés.`);
-
         console.log(`Vous demandez ${newFuel} litres`);
+        car1.newFuel = newFuel;
 
-        car1.addFuel = newFuel;
-
-        console.log(car1.errorFuel);
-        console.log(car1.dashboard);
+        console.log(car1.checkFuel());
+        console.log(car1.dashboard());
         break;
-      case "3":
-        console.log(`Vous préparez votre itinéraire`);
 
-        let distance = prompt("Combien de Km voulez-vous parcourir?");
+      case "3": // Se déplacer
+        if (car1.insurance == true) {
+          console.log(`Vous préparez votre itinéraire`);
 
-        console.log(`Vous voulez faire ${distance} Km`);
+          let distance = prompt("Combien de Km voulez-vous parcourir?");
+          console.log(`Vous voulez faire ${distance} Km`);
+          let speed = prompt("Entrez votre vitesse en Km/h");
+          console.log(`En roulant en moyenne à ${speed} Km/h`);
 
-        car1.distKm = distance;
+          let consumption = "";
+          if (speed < 50) {
+            consumption = (distance / 100) * 10;
+            console.log(`Vous allez consommer 10 litres aux 100km`);
+          } else if (50 <= speed && speed <= 90) {
+            consumption = (distance / 100) * 5;
+            console.log(`Vous allez consommer 5 litres aux 100km`);
+          } else if (91 <= speed && speed <= 130) {
+            consumption = (distance / 100) * 8;
+            console.log(`Vous allez consommer 8 litres aux 100km`);
+          } else if (131 <= speed) {
+            consumption = (distance / 100) * 12;
+            console.log(`Vous allez consommer 12 litres aux 100km`);
+          }
+          car1.consumption = consumption;
 
-        let speed = prompt("Entrez votre vitesse en Km/h");
-
-        console.log(`En roulant en moyenne à ${speed} Km/h`);
-
-        car1.speedKm = speed;
-
-        let consumption = "";
-        if (speed < 50) {
-          consumption = (distance / 100) * 10;
-          console.log(`Vous allez consommer 10 litres aux 100km`);
-        } else if (50 <= speed && speed <= 90) {
-          consumption = (distance / 100) * 5;
-          console.log(`Vous allez consommer 5 litres aux 100km`);
-        } else if (91 <= speed && speed <= 130) {
-          consumption = (distance / 100) * 8;
-          console.log(`Vous allez consommer 8 litres aux 100km`);
-        } else if (131 <= speed) {
-          consumption = (distance / 100) * 12;
-          console.log(`Vous allez consommer 12 litres aux 100km`);
+          console.log(car1.move());
+          console.log(car1.dashboard());
+        } else {
+          {
+            throw new Error(`
+        Vous vous êtes fait arreter par la police!
+        Vous recevez une amende de 90€ pour defaut d'assurance
+        Vous rentrez au garage (rafraichissez la page)`);
+          }
         }
-        car1.consumKm = consumption;
+        break;
 
-        alert(car1.move);
-        console.log(car1.dashboard);
+      case "4": // Prendre une assurance
+        if (car1.insurance == false) {
+          car1.insurance = true;
+          console.log(`Vous souscrivez à une assurance`);
+          console.log(car1.notice());
+          console.log(car1.dashboard());
+        } else {
+          alert(`Vous etes déja assuré!`);
+        }
         break;
     }
-  } else if (choice == 4) {
-    console.log(`Vous rentrez au garage`);
-    if (
-      confirm(`
-      OK pour terminer 
-      ou ANNULER pour recommencer l'opération`)
-    ) {
-      text = `
-      Merci d'avoir utilisé notre programme!
-      La ligue du Code vous salue bien!
-      Cliquez sur OK pour quitter`;
-
-      alert(text);
-    } else {
-      window.location.reload();
-    }
+  } else if (choice == 5) {
+    console.log(`
+    Vous rentrez au garage...
+    Merci d'avoir utilisé notre programme!
+    La ligue du Code vous salue bien!`);
   }
 }
